de45fa7132e84cd8de1a10ce511aa67135872c24
halomon3000 <halomon3000@gmail.com>
2026-07-07 18:42:36 -0500

    Adding Schedule CSS and structure

--- reactproject1/src/Pages/ScheduleSite.jsx
+++ reactproject1/src/Pages/ScheduleSite.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
+import './ScheduleSite.css';
+
 export function ScheduleSite() {
-        const today = new Date();
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [selectedDay, setSelectedDay] = useState(null);
+
+    const lastDay = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0).getDate();
+    
+    const divDates = [];
+
+    for (let i = 1; i <= lastDay; i++) {
+        divDates.push(
+        <div key={"Date" + i} className="date-box">
+            <h3>{i}</h3>
+        </div>
+        );
+    }
     return (
         <>
-            <h1>This is my Schedule</h1>
-            <h2>Today is today</h2>
+            <div className='schedule-header'>
+                <h1 className='yearText'> {currentDate.getFullYear()} </h1>
+            </div>
+            <div className='schedule-header'>
+                <h1 className='monthText'> {currentDate.toLocaleString('default', { month: 'long' })} </h1>
+            </div>        
+            <div className='schedule-grid'>
+                {divDates}
+            </div>
         </>
     )
 }
